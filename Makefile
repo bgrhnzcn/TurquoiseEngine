8738fd3dafe7aebff8625ff5dc9644b05b436185
bgrhnzcn <bugrahanozcan16@gmail.com>
2025-12-12 20:40:26 +0300

    base

--- Makefile
+++ Makefile
@@ -2,9 +2,13 @@ NAME = scop
 
 SHELL = /bin/bash
 
-COMPILER = c++
+CXX = c++
 
-FLAGS = -Wall -Werror -Wextra -std=c++2b
+CXX_FLAGS = -Wall -Werror -Wextra -std=c++2b -g
+
+CC = cc
+
+CC_FLAGS = -Wall -Werror -Wextra -g
 
 SRC_DIR = src
 
@@ -20,16 +24,30 @@ OBJS = $(SRCS:$(SRC_DIR)/%.cpp=$(OBJ_DIR)/%.o)
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
 	mkdir -p $(dir $@)
-	$(COMPILER) $(FLAGS) -I $(INC_DIR) $(GLFW_INC) -c $< -o $@
+	$(CXX) $(CXX_FLAGS) -I $(INC_DIR) -I $(GLFW_INC) -I $(GLAD_INC) -c $< -o $@
 
-C_SRCS = $(shell find $(SRC_DIR) -type f -name "*.c")
-
-C_OBJS = $(C_SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
-
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
-	mkdir -p $(dir $@)
-	$(COMPILER) $(FLAGS) -I $(INC_DIR) $(GLFW_INC) -c $< -o $@
 all: $(NAME)
+
+GLAD_DIR = $(LIB_DIR)/glad
+
+GLAD_INC = $(GLAD_DIR)
+
+GLAD_SRC = $(GLAD_DIR)/glad.c
+
+GLAD_OBJ = $(GLAD_SRC:.c=.o)
+
+GLAD_LINK = -L $(GLAD_DIR) -lglad
+
+GLAD = $(GLAD_DIR)/libglad.a
+
+$(GLAD): $(GLAD_OBJ)
+	$(AR) rcs $(GLAD) $(GLAD_OBJ)
+
+%.o: %.c
+	$(CC) $(CC_FLAGS) -I $(GLAD_INC) -c $< -o $@
+
+$(GLFW_DIR):
+	git clone https://github.com/glfw/glfw.git $(GLFW_DIR)
 
 ################################################################################
 #                                                                              #
@@ -39,9 +57,9 @@ all: $(NAME)
 
 GLFW_DIR = $(LIB_DIR)/glfw
 
-GLFW_INC = -I $(GLFW_DIR)/include/GLFW
+GLFW_INC = $(GLFW_DIR)/include/GLFW
 
-GLFW_LINK = -L $(GLFW_DIR)/src -lglfw3 -lGL -ldl -lpthread -lX11 -lXrandr 
+GLFW_LINK = -L $(GLFW_DIR)/src -lglfw3 -lGL -ldl -lpthread -lX11 -lXrandr
 
 GLFW = $(GLFW_DIR)/src/libglfw3.a
 
@@ -51,15 +69,17 @@ $(GLFW): $(GLFW_DIR)
 $(GLFW_DIR):
 	git clone https://github.com/glfw/glfw.git $(GLFW_DIR)
 
-$(NAME): $(GLFW) $(OBJS)
-	$(COMPILER) $(FLAGS) $(OBJS) $(GLFW_LINK) -o $@
+$(NAME): $(GLFW) $(GLAD) $(OBJS)
+	$(CXX) $(CXX_FLAGS) $(OBJS) $(GLAD_LINK) $(GLFW_LINK) -o $@
 
 clean:
-	rm -rf $(OBJ_DIR)
+	rm -rf $(OBJ_DIR) $(GLAD_OBJ)
 
 fclean: clean
-	rm -f $(NAME)
-	rm -rf $(LIB_DIR)
+	rm -f $(NAME) $(GLAD)
+
+hclean: fclean
+	rm -rf $(GLFW_DIR)
 
 re: fclean all
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "glad/glad.h"
-#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include "GLFW/glfw3.h"
 #include <iostream>
 
 void callback(GLFWwindow *win, int key, int scancode, int action, int mods) {
@@ -9,12 +9,25 @@ void callback(GLFWwindow *win, int key, int scancode, int action, int mods) {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
     glfwSetWindowShouldClose(win, GLFW_TRUE);
   if (action == GLFW_PRESS)
+		switch (key) {
+			case GLFW_KEY_Q:
+				glClearColor(1, 0, 0, 1);
+				break;
+			case GLFW_KEY_W:
+				glClearColor(0, 1, 0, 1);
+				break;
+			case GLFW_KEY_E:
+				glClearColor(0, 0, 1, 1);
+				break;
+			case GLFW_KEY_R:
+				glClearColor(1, 1, 1, 1);
+				break;
+		}
     std::cout << "key pressed: " << key << std::endl;
 }
 
-struct Vec3
-{
-	float x, y, z;
+struct Vec3 {
+  float x, y, z;
 };
 
 int main() {
@@ -36,27 +49,85 @@ int main() {
   */
 
   glfwDefaultWindowHints();
+	glfwWindowHint(GLFW_REPEAT, GLFW_FALSE);
 
   GLFWwindow *win = glfwCreateWindow(800, 600, "scope", nullptr, nullptr);
 
   if (win == nullptr)
-    std::cerr << "Failed to create window." << std::endl;
+	{
+		std::cerr << "Failed to create window." << std::endl;
+		exit(1);
+	}
+
+  glfwMakeContextCurrent(win);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to load OpenGL functions."<< std::endl;
+		exit(1);
+	}
+
+	glViewport(0, 0, 800, 600);
 
   glfwSetKeyCallback(win, callback);
 
+	glClearColor(0.34, 0.34, 0.34, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+// A simple triangle centered on screen
+Vec3 vertices[3] = {
+    {-0.5f, -0.5f, 0.0f}, // Bottom Left
+    { 0.5f, -0.5f, 0.0f}, // Bottom Right
+    { 0.0f,  0.5f, 0.0f}  // Top Center
+};
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+	
+	GLuint vao;
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(Vec3), &vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(0);
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+
+
+	const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1, 0.5, 0.2, 1);\n"
+    "}\0";
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+	unsigned int shader = glCreateProgram();
+	glAttachShader(shader, vertexShader);
+	glAttachShader(shader, fragmentShader);
+	glLinkProgram(shader);
+	glUseProgram(shader);
+
   while (!glfwWindowShouldClose(win)) {
-    glfwPollEvents();
+
+		glUseProgram(shader);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	glfwPollEvents();
+	glfwSwapBuffers(win);
   }
-
-	Vec3 vertices[3] = {{1, 1, 0},{0, 1, 0},{2, 1, 0}};
-
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(vbo, 3 * sizeof(Vec3), &vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-	glEnableVertexAttribArray(0);
-
+	glfwTerminate();
   return 0;
 }
