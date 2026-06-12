4adf4f5f5bc060fbd0d74d34d7c13f585cca462d
bgrhnzcn <bugrahanozcan16@gmail.com>
2026-06-12 18:59:32 +0300

    Updated .clang file. Added new assets for testing. Updated new namespace on main.

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,20 @@
 #include <GLFW/glfw3.h>
 // clang-format on
 
-#define KATIP_IMPLEMENTATION
-#include "logger.hpp"
-#undef KATIP_IMPLEMENTATION
-
-#include "mat4.hpp"
-#include "vec3.hpp"
-#include "test_engine.hpp"
+#include "core/math/mat4.hpp"
+#include "core/math/vec3.hpp"
+#include "core/test_engine.hpp"
+#include "core/util/logger.hpp"
 
 using Logger = ktp::Logger;
 
-lib::Mat4 translate = lib::Mat4::Translate({0, 0, 0});
+trq::Mat4 translate = trq::Mat4::Translate({0, 0, 0});
 
 int main()
 {
 	const Logger& log = Logger::getLogger();
 
-	TestEngine eng;
+	trq::TestEngine eng;
 	eng.init();
 	eng.run();
 	::glfwTerminate();
